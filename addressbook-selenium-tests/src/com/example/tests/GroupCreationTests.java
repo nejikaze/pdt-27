50c2ffcd238b9c0f114ffc33a891c92f7ffac61c
Aleksey <nejikaze.com@gmail.com>
2015-12-16 22:24:48 +0300

    pdt-dz-1

---
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -0,0 +1,30 @@
+package com.example.tests;
+
+import org.testng.annotations.Test;
+
+public class GroupCreationTests extends TestBase {
+	@Test
+	public void testNonEmptyGroupCreation() throws Exception {
+		openMainPage();
+		goToGroupsPage();
+		initGroupCreation();
+		GroupData group = new GroupData();
+		group.groupName = "group name test";
+		group.groupHeader = "group header test";
+		group.groupFooter = "group footer test";
+		fillGroupForms(group);
+		submitGroupCreation();
+		returnToGroupsPage();
+	}
+
+	@Test
+	public void testEmptyGroupCreation() throws Exception {
+		openMainPage();
+		goToGroupsPage();
+		initGroupCreation();
+		GroupData group = new GroupData("", "", "");
+		fillGroupForms(group);
+		submitGroupCreation();
+		returnToGroupsPage();
+	}
+}
