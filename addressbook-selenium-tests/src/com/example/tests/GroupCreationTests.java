876175cdcc18c2e008b3da7f71016d30f835d15f
Aleksey <nejikaze.com@gmail.com>
2015-12-28 17:57:17 +0300

    pdt-dz-5

--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -1,27 +1,49 @@
 package com.example.tests;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
 public class GroupCreationTests extends TestBase {
-	
+
 	@Test
 	public void testNonEmptyGroupCreation() throws Exception {
 		app.getNavigationHelper().openMainPage();
 		app.getNavigationHelper().goToGroupsPage();
+
+		// save old state
+		List<GroupData> oldList = app.getGroupHelper().getGroups();
+
+		// actions
 		app.getGroupHelper().initGroupCreation();
 		GroupData group = new GroupData();
-		group.name = "Name";
+		group.name = "aName";
 		group.header = "Header";
 		group.footer = "Footer";
 		app.getGroupHelper().fillGroupForms(group);
 		app.getGroupHelper().submitGroupCreation();
 		app.getGroupHelper().returnToGroupsPage();
+
+		// save new state
+		List<GroupData> newList = app.getGroupHelper().getGroups();
+
+		// compare states
+		oldList.add(group);
+		Collections.sort(oldList);
+		assertEquals(newList, oldList);
 	}
 
 	@Test
 	public void testEmptyGroupCreation() throws Exception {
 		app.getNavigationHelper().openMainPage();
 		app.getNavigationHelper().goToGroupsPage();
+
+		// save old state
+		List<GroupData> oldList = app.getGroupHelper().getGroups();
+
+		// actions
 		app.getGroupHelper().initGroupCreation();
 		GroupData group = new GroupData();
 		group.name = "";
@@ -30,5 +52,14 @@ public class GroupCreationTests extends TestBase {
 		app.getGroupHelper().fillGroupForms(group);
 		app.getGroupHelper().submitGroupCreation();
 		app.getGroupHelper().returnToGroupsPage();
+
+		// save new state
+		List<GroupData> newList = app.getGroupHelper().getGroups();
+
+		// compare states
+		oldList.add(group);
+		Collections.sort(oldList);
+		assertEquals(newList, oldList);
+
 	}
 }
